267ccdb8a11893cf5e50bd5a2630a4e6ea441de2
Harsh Rajput <harshhrajputgit@gmail.com>
2024-07-29 20:21:00 +0530

    35 to 46 done

--- P27.py
+++ P27.py
@@ -1,4 +1,4 @@
-# Q.27) How will you create a dictionary using tuples in python?
+# Q.27} How will you create a dictionary using tuples in python?
 
 
 dict_tuple = {('sunday',1),('monday',2),('tuesday',3)}
--- P29.py
+++ P29.py
@@ -1,4 +1,4 @@
-# Q.29)Write a Python script to concatenate following dictionaries to create a new one. 
+# Q.29}Write a Python script to concatenate following dictionaries to create a new one. 
 
 dict1 = {'a':1, 'b':2 }
 dict2 = {'c':3, 'd':4}
