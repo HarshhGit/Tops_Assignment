f4550650f7773a110b5f7b158e5e32919bf8ad3b
Harsh Rajput <harshhrajputgit@gmail.com>
2024-09-20 23:42:29 +0530

    updated directory

--- Practicals/P05.py
+++
@@ -1,9 +0,0 @@
-# Write a Python program to check a list is empty or not
-
-
-list1 = [5]
-
-if not list1:
-    print("The list is empty")
-else:
-    print("The list is not empty")
--- Practicals/P19.py
+++
@@ -1,6 +0,0 @@
-# 19) Write a Python program to find the length of a tuple.
-
-
-ex = ("hello", 5, 22, True, 3.14, None)
-
-print(len(ex))
